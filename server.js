30047ffdbc40f96b75818c27af91212e628dad70
dean <deanm1278@gmail.com>
2017-01-11 15:37:34 -0500

    updated for ssl

--- server.js
+++ server.js
@@ -1,5 +1,46 @@
 // set up ======================================================================
+'use strict';
 var express = require('express');
+
+// returns an instance of node-letsencrypt with additional helper methods
+var lex = require('letsencrypt-express').create({
+  // set to https://acme-v01.api.letsencrypt.org/directory in production
+  server: 'staging'
+
+// If you wish to replace the default plugins, you may do so here
+//
+, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
+, store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
+
+// You probably wouldn't need to replace the default sni handler
+// See https://github.com/Daplie/le-sni-auto if you think you do
+//, sni: require('le-sni-auto').create({})
+
+, approveDomains: approveDomains
+});
+
+function approveDomains(opts, certs, cb) {
+  // This is where you check your database and associated
+  // email addresses with domains and agreements and such
+
+
+  // The domains being approved for the first time are listed in opts.domains
+  // Certs being renewed are listed in certs.altnames
+  if (certs) {
+    opts.domains = certs.altnames;
+  }
+  else {
+    opts.email = 'deanm1278@gmail.com';
+    opts.agreeTos = true;
+  }
+
+  // NOTE: you can also change other options such as `challengeType` and `challenge`
+  // opts.challengeType = 'http-01';
+  // opts.challenge = require('le-challenge-fs').create({});
+
+  cb(null, { options: opts, certs: certs });
+}
+
 var app = express(); 						// create our app w/ express
 //var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port = process.env.PORT || 3000; 				// set the port
@@ -7,6 +48,10 @@ var port = process.env.PORT || 3000; 				// set the port
 var morgan = require('morgan');
 //var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+
+require('http').createServer(lex.middleware(require('redirect-https')())).listen(port, function () {
+  console.log("Listening for ACME http-01 challenges on", this.address());
+});
 
 // configuration ===============================================================
 //mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
@@ -23,5 +68,6 @@ app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-M
 require('./app/routes.js')(app);
 
 // listen (start app with node server.js) ======================================
-app.listen(port);
-console.log("App listening on port " + port);
+require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(3443, function () {
+  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+});
